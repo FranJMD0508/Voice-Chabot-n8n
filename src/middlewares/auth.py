cf47a0ac32935d56e7c01f44609ce81d8a054e49
FranJMD <fmejia22@ujap.edu.ve>
2025-06-12 15:40:25 -0400

    Método de autenticación para la API creado

---
+++ src/middlewares/auth.py
@@ -0,0 +1,7 @@
+from flask import request, jsonify
+from src.config import Config
+
+def check_api_key():
+    api_key_recibida = request.headers.get("API-KEY")
+    if not api_key_recibida or api_key_recibida != Config.API_KEY:
+        return jsonify({"error": "No Autorizado: Api Key no válida"}), 401
