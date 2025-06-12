e1721540e47e855692238c7bd160b5ec8193e929
FranJMD <fmejia22@ujap.edu.ve>
2025-06-12 15:58:08 -0400

    Ruta de conversión y middleware de autenticación registrados en el archivo principal de la API

--- src/app.py
+++ src/app.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from src.routes.audio_route import audio_bp
+from src.middlewares.auth import check_api_key
 
 app = Flask(__name__)
+
+app.before_request(check_api_key) # Middleware
+
+app.register_blueprint(audio_bp) # Ruta de audio
 
 app.run(host='0.0.0.0', port=5000, debug=True) # Al desplegar a produccion, cambiar debug a False
